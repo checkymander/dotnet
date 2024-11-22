829b56aee4f175493d1fd8d9b35626b3cdb45cf6
checkymander <checkymander@protonmail.com>
2024-11-22 14:14:00 +0000

    added workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ mkdir /opt/dotnet
 cd /opt/dotnet/
 git clone https://github.com/checkymander/dotnet .
 git switch release/8.0.1xx
-./prep.sh && ./build.sh --online
+./prep.sh && ./build.sh --online --clean-while-building
